f144086fe327e2b5da78c18cd49528d193cb9716
Alan Hannah <alan@queryclick.com>
2016-04-15 11:56:55 +0100

    add task to file, use one function to write to json file

--- todo.py
+++ todo.py
@@ -10,15 +10,17 @@ parser.add_argument('-d', '--delete_task', nargs=1, type=str)
 parser.add_argument('-t', '--toggle_task', nargs=1, type=str)
 
 def main():
-	if os.path.exists(settings.JSON_PATH):
-		json_data = load_json_data_from_file(settings.JSON_PATH)
-		import ipdb; ipdb.set_trace()
-	else:
-		create_default_json_file()
+	if not os.path.exists(settings.JSON_PATH):
+		json_data = create_default_json_dict()
+		write_json_file(json_data)
+
+	json_data = load_json_data_from_file(settings.JSON_PATH)
 
 	if args.add_task:
-		task_to_add = join_input(args.add_task)
-		add_task(task_to_add)
+		task_to_add = join_input(args.add_task, ' ')
+		task_dict = create_task(task_to_add)
+		updated_json_data = add_task(task_dict, json_data)
+		write_json_file(updated_json_data)
 
 	if args.delete_task:
 		print args.delete_task
@@ -34,16 +36,15 @@ def join_input(argument, connector):
 	:param connector:       a string to connect with
 	:returns joined_string: a string
 	"""
-	joined_string = argument.join(connector)
+	joined_string = connector.join(argument)
 	return joined_string 
 
-def create_default_json_file():
+def write_json_file(json_data):
 	"""
 	Create a default json file with the task list data
 
 	:calls create_default_json_dict:
 	"""
-	json_data = create_default_json_dict()
 	with open(settings.JSON_PATH, 'w+') as json_file:
 		json.dump(json_data, json_file)
 
@@ -68,17 +69,25 @@ def load_json_data_from_file(file_path):
 	"""
 	with open(file_path, 'r') as json_file:
 		tasks_dict = json.load(json_file)
-
 	return tasks_dict
 
-def add_task(task_string):
+def create_task(task_string):
+	task_dict = {
+		"status": "open",
+		"text": "{}".format(task_string),
+		"collapse": "false",
+	}
+	return task_dict
+
+def add_task(task_dict, json_data):
 	"""
 	Add a task to the json file. 
 
-	:param task_string:  a string to be added to the json file.
+	:param task_string: a string to be added to the json file.
+	:param json_data:   a dictionary of json data 
 	"""
-	pass
-
+	json_data['root']['children'].append(task_dict)
+	return json_data
 
 if __name__ == '__main__':
 	args = parser.parse_args()
